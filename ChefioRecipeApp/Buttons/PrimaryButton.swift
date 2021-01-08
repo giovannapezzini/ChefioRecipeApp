98e1cbebdf29d77d268c4671fc8732e3b4d3a861
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-08 03:58:33 -0300

    Add PrimaryButton

---
+++ ChefioRecipeApp/Buttons/PrimaryButton.swift
@@ -0,0 +1,33 @@
+//
+//  PrimaryButton.swift
+//  ChefioRecipeApp
+//
+//  Created by Giovanna Pezzini on 08/01/21.
+//
+
+import UIKit
+
+class PrimaryButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(title: String) {
+        self.init(frame: .zero)
+        self.backgroundColor = Colors.primaryColor
+        self.setTitle(title, for: .normal)
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 30;
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont(name: "Inter-Bold", size: 15)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}

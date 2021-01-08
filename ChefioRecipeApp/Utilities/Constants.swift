8d5c08138acbdb2e123e59c4bb0d8b465b79a60b
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-08 03:59:40 -0300

    Add constants

---
+++ ChefioRecipeApp/Utilities/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  ChefioRecipeApp
+//
+//  Created by Giovanna Pezzini on 08/01/21.
+//
+
+import UIKit
+
+enum Colors {
+    static let primaryColor = UIColor(red: 0.122, green: 0.8, blue: 0.475, alpha: 1)
+    static let secondaryColor = UIColor(red: 1, green: 0.392, blue: 0.392, alpha: 1)
+    static let mainTextColor = UIColor(red: 0.18, green: 0.242, blue: 0.363, alpha: 1)
+    static let secondaryTextColor = UIColor(red: 0.625, green: 0.648, blue: 0.754, alpha: 1)
+    static let outline = UIColor(red: 0.817, green: 0.857, blue: 0.917, alpha: 1)
+    static let form = UIColor(red: 0.957, green: 0.961, blue: 0.969, alpha: 1)
+}
+
+enum Images {
+    static let onboardingImage = UIImage(named: "Onboarding")
+}

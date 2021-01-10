814755d19040cab0f4be29382c379724dd51daca
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-10 19:23:06 -0300

    Add Firebase Authentication

--- ChefioRecipeApp/ViewControllers/SignUpViewController.swift
+++ ChefioRecipeApp/ViewControllers/SignUpViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class SignUpViewController: UIViewController {
     
@@ -18,12 +20,91 @@ class SignUpViewController: UIViewController {
     var moreThanSixChar = BodyLabel(textAlignment: .left, fontSize: 15)
     var containsNumber = BodyLabel(textAlignment: .left, fontSize: 15)
     var signUpButton = PrimaryButton(title: "Sign Up", backgroundColor: Colors.primaryColor)
+    var errorLabel = UILabel()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
         layoutUI()
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+    }
+    
+    func validateFields() -> String? {
+        print("validating")
+        
+        // Check that all fields are filled in
+        if nameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""  || emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            print("Please fill in all fields.")
+            return "Please fill in all fields."
+        }
+        
+        // Check if the password is secure
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if isPasswordValid(password) == false {
+            print("Please make sure your password is at least 8 characters, contains a special character and a number.")
+            return "Please make sure your password is at least 8 characters, contains a special character and a number."
+        }
+        
+        return nil
+    }
+    
+    func isPasswordValid(_ password : String) -> Bool{
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    @objc func signUpButtonTapped() {
+        print("tapped")
+        
+        // Validate the fields
+        let error = validateFields()
+        
+        if error != nil {
+            // There's something wrong with the fields, show error message
+            showError(error!)
+        } else {
+            // Create cleaned versions of the data
+            let name = nameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // Create the user
+            Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                // Check for errors
+                if error != nil {
+                    // There was an error creating the user
+                    self.showError("Error creating the user")
+                } else {
+                    // User was created successfully, now store the name
+                    let db = Firestore.firestore()
+                    db.collection("users").addDocument(data: ["name": name, "uid": result!.user.uid]) { (error) in
+                        if error != nil {
+                            self.showError("Error saving user data")
+                        }
+                    }
+                }
+            }
+            
+            // Transition to the home screen
+            transitionToHome()
+        }
+    }
+    
+    func transitionToHome() {
+        print("Home")
+    }
+    
+    func showError(_ message: String) {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+        
+        let ac = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default)
+        ac.addAction(action)
+        present(ac, animated: true)
     }
     
     func layoutUI() {

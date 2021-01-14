93e620ffc0912211aa155d2c0c6fc3dc24938139
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-14 02:22:00 -0300

    Refactor showAlert

--- ChefioRecipeApp/Extensions/UIViewController+Ext.swift
+++ ChefioRecipeApp/Extensions/UIViewController+Ext.swift
@@ -8,15 +8,23 @@
 import UIKit
 
 extension UIViewController {
-    func presentAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+    func showAlert(message: String) {
         DispatchQueue.main.async {
-            let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let ac = UIAlertController(title: "Oops!", message: message, preferredStyle: .alert)
             ac.modalPresentationStyle = .overFullScreen
             ac.modalTransitionStyle = .crossDissolve
             
             let action = UIAlertAction(title: "Ok", style: .default)
             ac.addAction(action)
             self.present(ac, animated: true)
+        }
+    }
+    
+    func performSegueToReturnBack()  {
+        if let nav = self.navigationController {
+            nav.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true, completion: nil)
         }
     }
 }

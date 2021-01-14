9cb58fa19bea70e47197d35b0c5c7ae3ce8be3fd
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-14 03:46:44 -0300

    Add HomeViewController

---
+++ ChefioRecipeApp/ViewControllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  ChefioRecipeApp
+//
+//  Created by Giovanna Pezzini on 14/01/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}

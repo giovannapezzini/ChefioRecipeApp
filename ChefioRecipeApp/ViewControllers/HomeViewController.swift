dca0f398af9c03ae2ff4d694a662d9baacdfbc62
giovannapezzini <giovannarpezzini@gmail.com>
2021-02-04 23:39:54 -0300

    Add collectionView

--- ChefioRecipeApp/ViewControllers/HomeViewController.swift
+++ ChefioRecipeApp/ViewControllers/HomeViewController.swift
@@ -7,11 +7,56 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var recipe = [Recipe]()
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
+        configureCollectionView()
+    }
+    
+    func setupView() {
+        title = "Home"
         view.backgroundColor = .white
+    }
+    
+    func configureCollectionView() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView?.register(RecipeCell.self, forCellWithReuseIdentifier: "RecipeCell")
+        collectionView?.backgroundColor = .systemGray5
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        view.addSubview(collectionView)
+    }
+    
+    // MARK: - DataSource methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCell", for: indexPath) as! RecipeCell
+        
+        cell.recipeImageView.image = UIImage(named: "image-placeholder")
+        cell.recipeLabel.text = "Recipe"
+        cell.timeLabel.text = "60 minutes"
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout
+        let space: CGFloat = (flowLayout?.minimumInteritemSpacing ?? 0.0) + (flowLayout?.sectionInset.left ?? 0.0) + (flowLayout?.sectionInset.right ?? 0.0)
+        let size: CGFloat = (collectionView.frame.size.width - space) / 2.0
+        return CGSize(width: size, height: size)
     }
 }

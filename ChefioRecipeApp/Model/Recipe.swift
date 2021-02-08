30f15cf671f25584aa3b00f365f9c5b0ba0bad06
giovannapezzini <giovannarpezzini@gmail.com>
2021-02-08 20:40:46 -0300

    Add CodingKeys and init method

--- ChefioRecipeApp/Model/Recipe.swift
+++ ChefioRecipeApp/Model/Recipe.swift
@@ -9,10 +9,30 @@ import Foundation
 
 struct Recipe: Codable, Hashable {
     var title : String?
-    var imageURL : String?
-    var timeRequired : Int?
-    var sourceURL : String?
-    var ingredients : [String]?
-    var instructions : [String]?
-    var servings: Int?
+    var image : String?
+    // var timeRequired : Int?
+    // var sourceURL : String?
+    // var ingredients : [String]?
+    // var instructions : [String]?
+    // var servings: Int?
+    
+    private enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case image = "image"
+        // case timeRequired = "timeRequired"
+        // case sourceURL = "sourceURL"
+        // case ingredients = "ingredients"
+        // case instructions = "instructions"
+        // case servings = "servings"
+    }
+    
+    init(from decoder: Decoder) throws {
+        if let container = try? decoder.container(keyedBy: CodingKeys.self) {
+            self.title = try container.decode(String.self, forKey: .title)
+            self.image = try container.decode(String.self, forKey: .image)
+        } else {
+            self.title = "title"
+            self.image = "image url"
+        }
+    }
 }

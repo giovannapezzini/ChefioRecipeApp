9fb30084cb076222f26f7e824e895ef97840acdf
giovannapezzini <giovannarpezzini@gmail.com>
2021-02-08 21:21:07 -0300

    Add titleTextAttributes

--- ChefioRecipeApp/Support/AppDelegate.swift
+++ ChefioRecipeApp/Support/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : Colors.mainTextColor]
         return true
     }
 

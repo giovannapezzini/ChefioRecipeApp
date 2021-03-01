c8d04d72b15b5c491cc128902f8de2ef9dbdc905
giovannapezzini <giovannarpezzini@gmail.com>
2021-03-01 15:04:04 -0300

    Add DetailRecipeViewController

---
+++ ChefioRecipeApp/View/Screens/DetailRecipeViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DetailRecipeViewController.swift
+//  ChefioRecipeApp
+//
+//  Created by Giovanna Pezzini on 28/02/21.
+//
+
+import UIKit
+
+class DetailRecipeViewController: UIViewController {
+    
+    // MARK:  - ScrollView
+        
+    lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.delaysContentTouches = false
+        view.bounces = true
+        return view
+    }()
+    
+    lazy var contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    // MARK:  - Properties
+    
+    let recipeImage = UIImageView()
+    
+    // MARK:  - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layoutUI()
+    }
+    
+    // MARK:  - Layout UI
+    
+    func layoutUI() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(recipeImage)
+            
+        let padding: CGFloat = 24
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        ])
+    }
+}
